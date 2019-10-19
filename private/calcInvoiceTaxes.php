a6ab5648448977ac0badc6cb0613e46d8c6da7d8
Andrew Rivett <veggiefrog@gmail.com>
2019-10-19 17:51:49 -0400

    Fixed calcInvoiceTaxes for use with preorders and shipping costs

--- private/calcInvoiceTaxes.php
+++ private/calcInvoiceTaxes.php
@@ -62,10 +62,17 @@ function ciniki_taxes_calcInvoiceTaxes($ciniki, $tnid, $invoice) {
         $tenant_taxes[$tid]['calculated_items_amount'] = 0;
         $tenant_taxes[$tid]['calculated_invoice_amount'] = 0;
     }
+    $preorder_taxes = $tenant_taxes;
 
     //
     // Go through the invoice items and calculate the taxes
     //
+    $invoice['items'][] = array(
+        'id'=>0,
+        'amount' => (isset($invoice['shipping_amount']) ? $invoice['shipping_amount'] : 0),
+        'preorder_amount' => (isset($invoice['preorder_shipping_amount']) ? $invoice['preorder_shipping_amount'] : 0),
+        'taxtype_id' => (isset($invoice['taxtype_id']) ? $invoice['taxtype_id'] : 0),
+        );
     foreach($invoice['items'] as $iid => $item) {
         foreach($tenant_taxes as $tid => $tax) {
             //
@@ -110,6 +117,49 @@ function ciniki_taxes_calcInvoiceTaxes($ciniki, $tnid, $invoice) {
                 }
             }
         }
+        foreach($preorder_taxes as $tid => $tax) {
+            //
+            // Check if the tax should be applied
+            //
+            if( isset($tax['types']) && is_array($tax['types']) 
+                && array_key_exists($item['taxtype_id'], $tax['types']) 
+                && $tax['location_id'] == $tax_location_id  // Double check tax_location_id
+                ) {
+                $preorder_taxes[$tid]['used'] = 'yes';
+                // 
+                // Calculate if tax is specified as included
+                //
+                if( ($tax['flags']&0x01) == 0x01 ) {
+                    if( $tax['item_percentage'] > 0 ) {
+                        //
+                        // For 13% tax, tax_amount = item_amount * (13/113)
+                        //
+                        $item_amount = bcmul($item['preorder_amount'], bcdiv($tax['item_percentage'], 100 + $tax['item_percentage'], 4), 4);
+                        $preorder_taxes[$tid]['calculated_items_amount'] = 
+                            bcadd($preorder_taxes[$tid]['calculated_items_amount'], $item_amount, 4);
+                    }
+                } 
+                //
+                // Calculate taxes extra
+                //
+                else {
+                    if( $tax['item_percentage'] > 0 ) {
+                        $item_amount = bcmul($item['preorder_amount'], bcdiv($tax['item_percentage'], 100, 6), 4);
+                        $preorder_taxes[$tid]['calculated_items_amount'] = 
+                            bcadd($preorder_taxes[$tid]['calculated_items_amount'], $item_amount, 4);
+                    }
+                    if( $tax['item_amount'] > 0 ) {
+                        $preorder_taxes[$tid]['calculated_items_amount'] = 
+                            bcadd($preorder_taxes[$tid]['calculated_items_amount'], 
+                                bcmul($item['quantity'], $tax['item_amount'], 4), 4);
+                    }
+                    if( $tax['invoice_amount'] > 0 ) {
+                        $preorder_taxes[$tid]['calculated_invoice_amount'] = 
+                            bcadd($preorder_taxes[$tid]['calculated_invoice_amount'], $tax['invoice_amount'], 4);
+                    }
+                }
+            }
+        }
     }
 
     //
@@ -120,7 +170,12 @@ function ciniki_taxes_calcInvoiceTaxes($ciniki, $tnid, $invoice) {
             unset($tenant_taxes[$tid]);
         }
     }
+    foreach($preorder_taxes as $tid => $tax) {
+        if( $preorder_taxes[$tid]['used'] == 'no' ) {
+            unset($preorder_taxes[$tid]);
+        }
+    }
 
-    return array('stat'=>'ok', 'taxes'=>$tenant_taxes);
+    return array('stat'=>'ok', 'taxes'=>$tenant_taxes, 'preorder_taxes'=>$preorder_taxes);
 }
 ?>
